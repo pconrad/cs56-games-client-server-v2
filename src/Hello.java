d0f632d3a7168b8980fddc25fc7b5769e6dbb86f
Phill Conrad <pconrad@cs.ucsb.edu>
2017-11-22 15:28:24 -0800

    add cmd line and german and spanish

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,20 @@
 public class Hello {
 
+	
+	
 	public static void main(String [] args) {
 
-		System.out.println("Hello, World!");
-
+		if (args < 1 || args[0].equals("EN")) {
+			System.out.println("Hello, World!");
+			return;
+		}
+		if (args[0].equals("DE")) {
+			System.out.println("Guten Tag!");
+		} else if (args[0].equals("ES")) {
+			System.out.println("Hola!");
+		} else {
+			System.out.println("Hello, World!");
+		}
 	}
 
 }
